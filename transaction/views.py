c2be2d57eac113c33d29e44cdbca519e631d9bc7
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2020-09-27 19:42:20 +0700

    add endpoint for create and get all transactions

--- transaction/views.py
+++ transaction/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.shortcuts import render
+from django.http import JsonResponse
+
+from item import models as item_models
+from . import models, serializers
+
+def index(req):
+  if (req.method == 'POST'):
+    return create(req)
+
+  transactions = models.Transaction.objects.all()
+  transactions = serializers.TransactionSerializer(transactions, many=True)
+  return JsonResponse(transactions.data, safe=False)
+
+def create(req):
+  transaction = models.Transaction.objects.create()
+  transaction_input = json.loads(req.body)
+  for item in transaction_input['detail']:
+    item_input = item_models.Item.objects.filter(pk=item['item_id']).first()
+    detail = models.TransactionDetail.objects.create(
+      transaction=transaction,
+      item=item_input,
+      qty=item['qty'],
+      note=item['note']
+    )
+  serializer = serializers.TransactionSerializer(transaction)
+  return JsonResponse(serializer.data, safe=False)
